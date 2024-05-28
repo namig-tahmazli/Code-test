b8df9b7dce10692054b66a14f522e108e977620b
Namig Tahmazli <ntehmezli@gmail.com>
2024-05-28 16:44:36 +0200

    Use platform specific activity indicator for iOS

--- shared/src/iosMain/kotlin/com/umain/test/fooddelivery/screens/ActivityIndicator.kt
+++ shared/src/iosMain/kotlin/com/umain/test/fooddelivery/screens/ActivityIndicator.kt
@@ -1,14 +1,8 @@
 package com.umain.test.fooddelivery.screens
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.interop.UIKitView
-import androidx.compose.ui.layout.onSizeChanged
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.UIKit.UIActivityIndicatorView
 import platform.UIKit.UIActivityIndicatorViewStyleMedium
@@ -18,11 +12,8 @@ import platform.UIKit.UIActivityIndicatorViewStyleMedium
 actual fun ActivityIndicator(
     modifier: Modifier
 ) {
-    var currentSize by remember { mutableStateOf(Size.Zero) }
     UIKitView(
-        modifier = modifier.onSizeChanged {
-            currentSize = Size(it.width.toFloat(), it.height.toFloat())
-        },
+        modifier = modifier,
         factory = {
             val indicator = UIActivityIndicatorView()
             indicator.setActivityIndicatorViewStyle(UIActivityIndicatorViewStyleMedium)
